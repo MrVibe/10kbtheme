ca29eac98977572e0130d22eeb353690485149bf
Madhvendra <madhvendra@99fusion.com>
2018-10-09 13:31:07 +0530

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<title><?php wp_title(); ?></title>
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	THIS is a 10kb theme
+</body>
+</html>
